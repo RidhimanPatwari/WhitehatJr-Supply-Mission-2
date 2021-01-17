8d62719e5cc7d66557e69a9a3be944fdd72d89dc
RidhimanPatwari <65568269+RidhimanPatwari@users.noreply.github.com>
2021-01-16 18:42:28 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,9 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG, boxSpriteLeft, boxSpriteRight, boxSpriteDown;
-var packageBody, boxBodyLeft, boxBodyRight, boxBodyDown, ground, groundSprite;
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-var building1, building2, building3, building4, building5, building6, building7, building8;
 
 function preload()
 {
@@ -18,36 +17,49 @@ function setup() {
 	
 
 	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG);
-	packageSprite.scale=0.2;
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
 
 	helicopterSprite=createSprite(width/2, 200, 10,10);
 	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6;
-
+	helicopterSprite.scale=0.6
 
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
-	
-	 
+
+
 	engine = Engine.create();
-    world = engine.world;
-    
-    packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
-    World.add(world, packageBody);
-    
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+	
 
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
-    World.add(world, ground);
+ 	World.add(world, ground);
+
+ 	boxPosition=width/2-100
+ 	boxY=610;
 
 
-	
-	boxSpriteDown = createSprite(400, 760, 200, 20);
-    boxSpriteDown.shapeColor = color(255, 0, 0);
-    
-    boxBodyDown = Bodies.rectangle(boxSpriteDown.x, boxSpriteDown.y, 200,20, {isStatic:true} );
-    World.add(world, boxBodyDown);
+ 	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxLeftBody);
+
+ 	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+ 	boxBase.shapeColor=color(255,0,0);
+
+ 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+ 	World.add(world, boxBottomBody);
+
+ 	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);
 
 
 
